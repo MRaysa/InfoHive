eaf67c2391686a1fca722e00bdbac54b586fa294
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-05-04 04:19:48 +0600

    fiarbase env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZsdARyYF5df_p3YynBF5ho0i6A4yE3x8",
-  authDomain: "infohive-6269a.firebaseapp.com",
-  projectId: "infohive-6269a",
-  storageBucket: "infohive-6269a.firebasestorage.app",
-  messagingSenderId: "1019098432756",
-  appId: "1:1019098432756:web:9522ef9f94ca4b798f0aa1",
+  // apiKey: "AIzaSyCZsdARyYF5df_p3YynBF5ho0i6A4yE3x8",
+  // authDomain: "infohive-6269a.firebaseapp.com",
+  // projectId: "infohive-6269a",
+  // storageBucket: "infohive-6269a.firebasestorage.app",
+  // messagingSenderId: "1019098432756",
+  // appId: "1:1019098432756:web:9522ef9f94ca4b798f0aa1",
+
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
